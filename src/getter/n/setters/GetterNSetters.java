b57222ea6041a7e12a3e1bd6fbe581b06445b10c
Wh-s-code <ST10492679@rcconnect.edu.za>
2025-04-10 08:48:34 +0000

    Initial commit

---
+++ src/getter/n/setters/GetterNSetters.java
@@ -0,0 +1,21 @@
+package getter.n.setters;
+
+import java.util.Scanner;
+
+public class GetterNSetters {
+
+    public static void main(String[] args) {
+//Object calling
+    Scanner scan = new Scanner(System.in);
+    Login Log = new Login();
+    
+    System.out.print("Enter a name: ");
+    String name = scan.nextLine();
+//setter
+    Log.setName(name); //use it to retrieve the value
+    
+//Dispaly
+    System.out.print("Name is " + Log.getName()); 
+    }
+    
+}
---
+++ src/getter/n/setters/Login.java
@@ -0,0 +1,52 @@
+package getter.n.setters;
+
+public class Login {//Start Class
+//Declaration
+private String name;
+
+//Contructor
+public Login(){
+
+}
+
+//void doesnt return a value
+ public void setName(String Name){//Start method
+    name = Name;
+     }// End method
+
+ 
+public String getName(){//Value returning method
+ return name; 
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }// End class 
+    
